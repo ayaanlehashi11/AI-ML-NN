729ad2ca19501fd342b33f5014ec96242e197598
0dayx16 <91608871+ayaanlehashi11@users.noreply.github.com>
2023-12-13 09:18:18 +0300

    Create Autoencoder.py

---
+++ neural_network/autoencoders/Autoencoder.py
@@ -0,0 +1,14 @@
+
+# import the necessary packages
+import imageio
+import glob
+import os
+import time
+import cv2
+import tensorflow as tf
+from tensorflow.keras import layers
+from IPython import display
+import matplotlib.pyplot as plt
+import numpy as np
+%matplotlib inline
+from tensorflow import keras
