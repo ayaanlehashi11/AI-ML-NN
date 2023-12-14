ce753ba21ff747fda8859415875698ba0791dfe8
0dayx16 <91608871+ayaanlehashi11@users.noreply.github.com>
2023-12-14 17:55:14 +0300

    Update LDA.py

--- supervised_learning/classification/LDA.py
+++ supervised_learning/classification/LDA.py
@@ -1,1 +1,26 @@
 
+import numpy as np 
+from sklearn.discriminant_analysis 
+import LinearDiscriminantAnalysis 
+from sklearn.model_selection import train_test_split 
+
+# Generate random data 
+X = np.random.randn(100, 10) 
+y = np.random.randint(2, size=100) 
+
+# Split the data into training and test sets 
+X_train, X_test,\ 
+	y_train, y_test = train_test_split(X, y, 
+									test_size=0.3) 
+
+# Create a LinearDiscriminantAnalysis estimator 
+# and fit it to the training data 
+estimator = LinearDiscriminantAnalysis(shrinkage=None) 
+estimator.fit(X_train, y_train) 
+
+# Obtain predictions for the test set 
+y_pred = estimator.predict(X_test) 
+
+# Print the classification accuracy 
+print(estimator.score(X_test, y_test)) 
+
