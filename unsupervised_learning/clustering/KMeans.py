9a1037d507235fd6fc09dafe9b3ef13f7542886f
0dayx16 <91608871+ayaanlehashi11@users.noreply.github.com>
2023-12-25 12:58:33 +0300

    Update KMeans.py

--- unsupervised_learning/clustering/KMeans.py
+++ unsupervised_learning/clustering/KMeans.py
@@ -82,15 +82,4 @@ plt.title("Unevenly Sized Blobs \nwith several initializations")
 plt.show()
 
 
-fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
 
-y_pred = GaussianMixture(n_components=3).fit_predict(X_aniso)
-ax1.scatter(X_aniso[:, 0], X_aniso[:, 1], c=y_pred)
-ax1.set_title("Anisotropically Distributed Blobs")
-
-y_pred = GaussianMixture(n_components=3).fit_predict(X_varied)
-ax2.scatter(X_varied[:, 0], X_varied[:, 1], c=y_pred)
-ax2.set_title("Unequal Variance")
-
-plt.suptitle("Gaussian mixture clusters").set_y(0.95)
-plt.show()
